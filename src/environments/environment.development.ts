77ca6b6e1f6d99ce589b863318df4eb7f4bb6713
Star-xxy <xinyan4646@163.com>
2024-10-13 12:18:08 +0800

    commit 11 change the  directory

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    apiBaseUrl: 'http://localhost:3000/api' // 开发环境 API
-  };
-  
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    apiBaseUrl: 'http://24274588.it.scu.edu.au/api' // 生产环境 API
-  };
-  
